ca57d98fac7012bdd36bcaaccdcb7985e9d3287d
gerritmulder <gmuld@live.nl>
2019-09-17 23:36:50 +0200

    Add templates sln and XAF .NET Core 3 WinForms template

---
+++ templates/XafNetCore3WinForms/Program.cs
@@ -0,0 +1,34 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.ExpressApp.Win;
+using System;
+
+namespace XafNetCore3WinForms
+{
+    internal class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            ImageLoader.Instance.UseSvgImages = true;
+            using var app = new WinApplication()
+            {
+                UseLightStyle = true,
+                UseOldTemplates = false
+            };
+
+            app.CreateCustomObjectSpaceProvider += (s, e)
+                => e.ObjectSpaceProvider = new NonPersistentObjectSpaceProvider();
+
+            try
+            {
+                app.Setup();
+                app.Start();
+            }
+            catch (Exception ex)
+            {
+                app.HandleException(ex);
+            }
+        }
+    }
+}
